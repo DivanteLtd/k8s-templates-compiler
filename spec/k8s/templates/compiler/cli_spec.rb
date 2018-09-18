4c0ece37609e3bf145d1d6cf67dd5d960555b9d6
Mateusz Koszutowski <mkoszut@gmail.com>
2018-09-18 15:59:56 +0200

    Compile all namespaces

--- spec/k8s/templates/compiler/cli_spec.rb
+++ spec/k8s/templates/compiler/cli_spec.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'fileutils'
+
 RSpec.describe K8s::Templates::Compiler::Cli do
   it 'has method run' do
     expect(K8s::Templates::Compiler::Cli.method_defined?(:run)).to eq(true)
@@ -13,5 +15,40 @@ RSpec.describe K8s::Templates::Compiler::Cli do
   it 'has no environment' do
     compiler = K8s::Templates::Compiler::Cli.new
     expect(compiler.run).to eq(1)
+  end
+
+  context 'environments' do
+    before(:context) do
+      @tmp_dir = '.tmp'
+      @config_dir = @tmp_dir + '/config'
+
+      @environments = %w[test1 test2]
+
+      FileUtils.rm_rf(@tmp_dir)
+      FileUtils.mkdir_p(@config_dir)
+      @environments.each do |environment|
+        FileUtils.mkdir_p(@config_dir + '/' + environment)
+      end
+    end
+
+    it 'has defined environment' do
+      compiler = K8s::Templates::Compiler::Cli.new
+
+      expect(compiler).to receive(:run_compilation).once
+
+      compiler.run(['--environment', 'dev', '-c', 'tmp/config'])
+    end
+
+    it 'has all-environment option' do
+      compiler = K8s::Templates::Compiler::Cli.new
+
+      expect(compiler).to receive(:run_compilation).twice
+
+      compiler.run(['--all-environments', '-c', 'tmp/config'])
+    end
+
+    after(:context) do
+      FileUtils.rm_rf(@tmp_dir)
+    end
   end
 end
